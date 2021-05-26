6f06f651c70597f736f9c056f9b944ce7b206624
Danielle Smalley <smalley.danielle@gmail.com>
2021-05-26 13:03:01 -0500

    Student class complete

---
+++ ClassesLibrary/Students.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        //fields
+        private string _firstName;
+        private string _lastName;
+
+
+        //properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }//end firstname
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }//end lastname
+
+        //ctors
+
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Student()
+        {
+
+        }
+
+        //methods
+
+        public override string ToString()
+        {
+            return string.Format("First Name: {0}\t\tLast Name: {1}", FirstName, LastName);
+        }
+
+    }//end class
+}//end namespace
---
+++ TesterProgram/TesterProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesLibrary;
+
+namespace TesterProgram
+{
+    class TesterProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n-----Student Info-----\n\n");
+
+
+            Student s1 = new Student("Spongebob", "Squarepants");
+            Console.WriteLine(s1);
+
+            Student s2 = new Student();
+            s2.FirstName = "Patrick";
+            s2.LastName = "Star";
+            Console.WriteLine(s2);
+
+        } //end main
+
+        private class Students
+        {
+        }
+    }//end class
+}//end namespace
