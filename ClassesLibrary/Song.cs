10e3d77f2119f5fe9b7c3de48dac0c372b844a8b
Danielle Smalley <smalley.danielle@gmail.com>
2021-05-27 16:11:42 -0500

    Initial commit

---
+++ ClassesLibrary/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Song
+    {
+        //fields
+        //props
+        //ctors
+        //methods
+
+    }//end class
+}//end namespace
