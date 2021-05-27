7e65e2718d8f7eff5d017eecdc8a3b29c42cb661
Danielle Smalley <smalley.danielle@gmail.com>
2021-05-27 17:15:36 -0500

    Initial Commit

---
+++ ClassesLibrary/MotorHome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class MotorHome
+    {
+        //fields
+        //props
+        //ctors
+        //methods
+
+    }//end class
+}//end namespace
