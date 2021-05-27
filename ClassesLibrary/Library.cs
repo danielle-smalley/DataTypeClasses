fef6938ba9bfa41058062d259af2f4c7cd7315db
Danielle Smalley <smalley.danielle@gmail.com>
2021-05-27 15:46:05 -0500

    Initial Commit

--- ClassesLibrary/Library.cs
+++ ClassesLibrary/Library.cs
@@ -8,6 +8,18 @@ namespace ClassesLibrary
 {
     public class Library
     {
+        //fields - N/A
+
+        //props
+        public List<Book> Books { get; set; }
+        public string LibraryName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+        //ctors
+        //methods
 
     }//end class
 }//end namespace
